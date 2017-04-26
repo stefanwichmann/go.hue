fd1452b7ddff3634066203ff1fb256f88e6d190e
Stefan Wichmann <mail@stefanwichmann.com>
2017-04-26 18:28:29 +0200

    Add license header

--- upnp.go
+++ upnp.go
@@ -1,20 +1,26 @@
+// MIT License
+//
+// Copyright (c) 2017 Stefan Wichmann
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 package hue
 
-/*
-Response example:
-
-HTTP/1.1 200 OK
-HOST: 239.255.255.250:1900
-EXT:
-CACHE-CONTROL: max-age=100
-LOCATION: http://192.168.178.241:80/description.xml
-SERVER: FreeRTOS/7.4.2 UPnP/1.0 IpBridge/1.10.0
-hue-bridgeid: 001788FFFE09A206
-ST: upnp:rootdevice
-USN: uuid:2f402f80-da50-11e1-9b23-00178809a206::upnp:rootdevice
-
-FROM: https://developers.meethue.com/documentation/changes-bridge-discovery
-*/
 import "time"
 import "net"
 import "strings"
@@ -82,6 +88,22 @@ loop:
 }
 
 func ssdpResponseValid(body string, origin net.IP) (valid bool, err error) {
+	/*
+		Response example:
+
+		HTTP/1.1 200 OK
+		HOST: 239.255.255.250:1900
+		EXT:
+		CACHE-CONTROL: max-age=100
+		LOCATION: http://192.168.178.241:80/description.xml
+		SERVER: FreeRTOS/7.4.2 UPnP/1.0 IpBridge/1.10.0
+		hue-bridgeid: 001788FFFE09A206
+		ST: upnp:rootdevice
+		USN: uuid:2f402f80-da50-11e1-9b23-00178809a206::upnp:rootdevice
+
+		FROM: https://developers.meethue.com/documentation/changes-bridge-discovery
+	*/
+
 	// Validate header
 	if !strings.Contains(body, "HTTP/1.1 200 OK") {
 		return false, errors.New("Invalid SSDP response header")
