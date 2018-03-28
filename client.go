09ed4f40be136df34715457d325b5b1672ac154d
Stefan Wichmann <mail@stefanwichmann.com>
2018-03-28 16:35:45 +0200

    Rewrite of client logic to support HTTPS

--- client.go
+++ client.go
@@ -1,28 +1,34 @@
 package hue
 
-import (
-	"net"
-	"net/http"
-	"time"
-)
+import "crypto/tls"
+import "net"
+import "net/http"
+import "time"
 
-var client *http.Client = newTimeoutClient(2*time.Second, 2*time.Second)
+// Use a global timeout for all client operations
+const clientTimeoutInSeconds = 2 * time.Second
 
-func timeoutDialer(connectTimeout time.Duration, readWriteTimeout time.Duration) func(net, addr string) (c net.Conn, err error) {
-	return func(netw, addr string) (net.Conn, error) {
-		conn, err := net.DialTimeout(netw, addr, connectTimeout)
-		if err != nil {
-			return nil, err
-		}
-		conn.SetDeadline(time.Now().Add(readWriteTimeout))
-		return conn, nil
+func newTimeoutClient() *http.Client {
+	transport := http.Transport{
+		Dial:                  timeoutDialer,
+		DialTLS:               timeoutDialerTLS,
+		TLSHandshakeTimeout:   clientTimeoutInSeconds,
+		ResponseHeaderTimeout: clientTimeoutInSeconds,
+	}
+
+	return &http.Client{
+		Transport: &transport,
+		Timeout:   clientTimeoutInSeconds,
 	}
 }
 
-func newTimeoutClient(connectTimeout time.Duration, readWriteTimeout time.Duration) *http.Client {
-	return &http.Client{
-		Transport: &http.Transport{
-			Dial: timeoutDialer(connectTimeout, readWriteTimeout),
-		},
-	}
+func timeoutDialer(network, addr string) (net.Conn, error) {
+	dialer := net.Dialer{Timeout: clientTimeoutInSeconds}
+	return dialer.Dial(network, addr)
+}
+
+func timeoutDialerTLS(network, addr string) (net.Conn, error) {
+	// The hue bridge uses a self-signed certificate
+	conf := tls.Config{InsecureSkipVerify: true}
+	return tls.Dial(network, addr, &conf)
 }
